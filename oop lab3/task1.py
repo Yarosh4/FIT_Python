298c4593846b8badf9b3e75f72deb380c20b362d
Yarosh4 <147854782+Yarosh4@users.noreply.github.com>
2023-12-26 19:41:32 +0300

    Add files via upload

---
+++ oop lab3/task1.py
@@ -0,0 +1,50 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        """
+        использован префикс __ для обозначения приватности,
+        чтобы не позволить менять атриибуты.
+        Для обращения необходимо использовать _Book__name.
+        По идеи надо было использовать protected, но с префиксом _
+        все еще можно менять значение атрибута для экземпляра
+        """
+        self.__name = name
+        self.__author = author
+
+    def __str__(self):
+        return f"Книга {self.__name}. Автор {self.__author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.__name!r}, author={self.__author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        if not isinstance(pages, int):
+            raise TypeError("Количество страниц должно быть целочисленным")
+        if pages <= 0:
+            raise ValueError("Количество страниц должно быть положительным")
+        self.pages = pages
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._Book__name!r}, author={self._Book__author!r}, pages={self.pages})"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        if not isinstance(duration, float):
+            raise TypeError("Продолжительность должна быть типом float")
+        if duration <= 0:
+            raise ValueError("Продолжительность должна быть положительна")
+        self.duration = duration
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._Book__name!r}, author={self._Book__author!r}, duration={self.duration})"
+
+
+audio = AudioBook("master", "Bulgakov", 1.05)
+paper = PaperBook("war", "Tolst", 450)
+print(audio.__repr__())
+print(paper.__repr__())
